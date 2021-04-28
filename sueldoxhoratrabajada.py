82f15105e468ab662f16ce7b7647fba7c7847355
nacholl51 <66977385+nacholl51@users.noreply.github.com>
2021-04-28 17:49:08 -0300

    Create sueldoxhoratrabajada.py

---
+++ sueldoxhoratrabajada.py
@@ -0,0 +1,5 @@
+horas_trabajadas=int(input("Ingresar horas trabajadas:"))
+valor_hora=int(input("Ingrese precio por hora:"))
+sueldo=horas_trabajadas * valor_hora
+print("El sueldo a pagar es:")
+print(sueldo)
